dc3042697895ab1df77312b48dda15dd70851f63
miguelgz <mmiguel360360@outlook.com>
2023-03-27 22:47:13 -0500

    added structure

---
+++ configurations/server.go
@@ -0,0 +1,15 @@
+package configurations
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func InitServer() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	http.ListenAndServe(":8080", r)
+}
---
+++ mamuro.go
@@ -0,0 +1,5 @@
+package mamurosearchservice
+
+func main() {
+
+}
