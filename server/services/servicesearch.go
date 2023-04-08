65d9c21586dfa05968b6cfe372e6ba6b59e0ff29
miguelgz <mmiguel360360@outlook.com>
2023-04-08 14:40:44 -0500

    fixed query

--- server/services/servicesearch.go
+++ server/services/servicesearch.go
@@ -51,7 +51,7 @@ func Search(textToSearch string) []byte {
 
 func getJsonBodyQuery(textToSearch string) map[string]interface{} {
 	query_string := map[string]interface{}{
-		"query": "\"" + textToSearch + "\"",
+		"query": "message:equals('" + textToSearch + "')",
 	}
 
 	must := []map[string]interface{}{
@@ -85,7 +85,7 @@ func getJsonBodyQuery(textToSearch string) map[string]interface{} {
 
 	result := map[string]interface{}{
 		"query": query,
-		"size":  2,
+		"size":  10,
 		"from":  0,
 		"sort":  sort,
 		"aggs":  aggs,
